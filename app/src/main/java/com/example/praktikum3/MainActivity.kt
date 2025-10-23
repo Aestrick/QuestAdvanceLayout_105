33e3007772137cea92ffafb05efd2ef514069a01
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-23 15:51:40 +0700

    Menambahkan beberapa kode di MainActivity.kt

--- app/src/main/java/com/example/praktikum3/MainActivity.kt
+++ app/src/main/java/com/example/praktikum3/MainActivity.kt
@@ -1,47 +1,29 @@
-package com.example.praktikum3
+package com.example.praktikum3 // Package Anda dari screenshot
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+// Import Tema Anda dari folder ui.theme
 import com.example.praktikum3.ui.theme.Praktikum3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            // Nama Tema Anda (kemungkinan Praktikum3Theme)
             Praktikum3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // MENGGANTI "Greeting("Android")" DENGAN FUNGSI ANDA
+                    ActivitasPertama(modifier = Modifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum3Theme {
-        Greeting("Android")
     }
 }
