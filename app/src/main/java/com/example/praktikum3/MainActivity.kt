aa2db11ef0f322b820ccb9ee9a89c2c3249f118f
Nashrul Fikri <nashrul.fikri19@gmail.com>
2025-10-23 15:54:42 +0700

    Menghapus space diantara import androidx dan com.example

--- app/src/main/java/com/example/praktikum3/MainActivity.kt
+++ app/src/main/java/com/example/praktikum3/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-
 import com.example.praktikum3.ui.theme.Praktikum3Theme
 
 class MainActivity : ComponentActivity() {
